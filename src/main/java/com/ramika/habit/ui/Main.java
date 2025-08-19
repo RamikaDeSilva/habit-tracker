186f96c9a46aa0e025c65f5e304b70c209d05078
Ramika De Silva <ramikadesilva0@gmail.com>
2025-08-18 18:59:30 -0700

    Linked Main to UserApp Console

--- src/main/java/com/ramika/habit/ui/Main.java
+++ src/main/java/com/ramika/habit/ui/Main.java
@@ -1,5 +1,7 @@
 package main.java.com.ramika.habit.ui;
 
 public class Main {
-
+    public static void main(String[] args) {
+        new UserApp();
+    }
 }
--- src/main/java/com/ramika/habit/ui/UserApp.java
+++ src/main/java/com/ramika/habit/ui/UserApp.java
@@ -2,4 +2,7 @@ package main.java.com.ramika.habit.ui;
 
 public class UserApp {
 
+    public UserApp() {
+        System.out.println("hello man! ");
+    }
 }
