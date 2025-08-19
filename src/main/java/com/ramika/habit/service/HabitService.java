91c6b080ce7f857a9ec7cb1ea57e693c0736cee5
Ramika De Silva <ramikadesilva0@gmail.com>
2025-08-18 17:25:27 -0700

    Specification for HabitService.java

--- src/main/java/com/ramika/habit/service/HabitService.java
+++ src/main/java/com/ramika/habit/service/HabitService.java
@@ -1,4 +1,38 @@
 package main.java.com.ramika.habit.service;
 
+import main.java.com.ramika.habit.model.Habit;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
+
+// Singleton class to handle all habit functionality
 public class HabitService {
+    private HabitService instance;
+    private static Map<UUID, Habit> allHabits;
+    private static Map<UUID, Habit> activeHabits;
+
+    private HabitService() {
+        allHabits = new LinkedHashMap<>();
+        activeHabits = new LinkedHashMap<>();
+    }
+
+    public HabitService getInstance() {
+        if (instance != null) {
+            return instance;
+        }
+        instance = new HabitService();
+        return instance;
+    }
+
+    // MODIFIES: allHabits, activeHabits
+    // EFFECTS: Creates new habit with given name, priority, category
+    public void createHabit() {
+    }
+
+    // MODIFIES: allHabits, activeHabits
+    // EFFECTS: Removes habit from allHabits and activeHabits if active
+    // if not in allHabits, throw exception - CREATE ONE FOR THIS!!!
+    public void deleteHabit() {
+    }
 }
--- src/main/java/com/ramika/habit/ui/UserApp.java
+++ src/main/java/com/ramika/habit/ui/UserApp.java
@@ -1,4 +1,5 @@
 package main.java.com.ramika.habit.ui;
 
 public class UserApp {
+
 }
