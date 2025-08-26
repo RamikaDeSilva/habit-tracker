a85b962385bbb3d461974b54318687cf4523d9aa
Ramika De Silva <ramikadesilva0@gmail.com>
2025-08-26 11:36:56 -0700

    Added SummaryService.java

--- src/main/java/com/ramika/habit/service/SummaryService.java
+++ src/main/java/com/ramika/habit/service/SummaryService.java
@@ -1,5 +1,22 @@
 package main.java.com.ramika.habit.service;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 // Class that deals with math calculations for habits and summarizations
 public class SummaryService {
+    private Map<Long, Map<LocalDate, Boolean>> habitCompletionMap = new HashMap<>();
+
+    public void markHabitCompleted(Long habitId, LocalDate date) {
+        habitCompletionMap
+                .computeIfAbsent(habitId, k -> new HashMap<>())
+                .put(date, true);
+    }
+
+    public boolean isHabitCompleted(Long habitId, LocalDate date) {
+        return habitCompletionMap
+                .getOrDefault(habitId, new HashMap<>())
+                .getOrDefault(date, false);
+    }
 }
