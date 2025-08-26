dbd7b07d1f42eb703110fc975fd8284c812ed65e
Ramika De Silva <ramikadesilva0@gmail.com>
2025-08-26 11:42:30 -0700

    Added HabitCompletion.java to store days habits are completed

---
+++ src/main/java/com/ramika/habit/model/HabitCompletion.java
@@ -0,0 +1,29 @@
+package main.java.com.ramika.habit.model;
+
+import java.time.LocalDate;
+
+public class HabitCompletion {
+    private LocalDate date;
+    private boolean completed;
+
+    public HabitCompletion(Long habitId, LocalDate date, boolean completed) {
+        this.date = date;
+        this.completed = completed;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
